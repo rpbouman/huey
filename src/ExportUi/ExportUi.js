a5c769cc60660234008b6e3c90a6ebd358d0e24b
Roland Bouman <Roland.Bouman@gmail.com>
2024-01-26 11:26:46 +0100

    Status info for the export dialog.

--- src/ExportUi/ExportUi.js
+++ src/ExportUi/ExportUi.js
@@ -129,141 +129,150 @@ function updateExportDialog(){
 }
 
 async function executeExport(){
-  var title = byId('exportTitle').innerText;
-  
-  var sql, structure;
-  if (byId('exportResultShapePivot').checked){
-    structure = 'pivot';
-    sql = getDuckDbPivotSqlStatementForQueryModel(queryModel);
-  }
-  else
-  if (byId('exportResultShapeTable').checked){
-    structure = 'table';
-    sql = getDuckDbTableSqlStatementForQueryModel(queryModel);
-  }
+  var dom = byId('exportDialog');
+  dom.setAttribute('aria-busy', String(true));
+  var progressMessageElement = dom.querySelector('*[role=progressbar] *[role=status]');
+  progressMessageElement.innerText = 'Preparing export...';
+  try {
+    var title = byId('exportTitle').innerText;
     
-  var selectedTab = TabUi.getSelectedTab('#exportDialog');
-  var tabName = selectedTab.getAttribute('for');
-  
-  var mimeType, compression, includeHeaders,
-      dateFormat, timestampFormat, nullValueString,
-      columnDelimiter, quote, escape, rowDelimiter
-  ;
-  var fileExtension, data, copyStatementOptions;
-  switch (tabName) {
-    case 'exportDelimited':
-      columnDelimiter = byId(tabName + 'ColumnDelimiter').value;
-      nullValueString = byId(tabName + 'NullString').value;
-      includeHeaders = byId(tabName + 'IncludeHeaders').checked;
-      quote = byId(tabName + 'Quote').value;
-      escape = byId(tabName + 'Escape').value;
-      dateFormat = byId(tabName + 'DateFormat').value;
-      timestampFormat = byId(tabName + 'TimestampFormat').value;
-      compression = byId(tabName + 'Compression').value;
-      copyStatementOptions = {
-        "FORMAT": 'csv',
-        "DELIMITER": `'${columnDelimiter.replace('\'', "''")}'`,
-        "NULL": `'${nullValueString.replace('\'', "''")}'`,
-        "HEADER": includeHeaders ? 'TRUE' : 'FALSE',
-        "QUOTE": `'${quote.replace('\'', "''")}'`,
-        "ESCAPE": `'${escape.replace('\'', "''")}'`,
-        "DATEFORMAT": `'${dateFormat.replace('\'', "''")}'`,
-        "TIMESTAMPFORMAT": `'${timestampFormat.replace('\'', "''")}'`,
-        "COMPRESSION": `'${compression.replace('\'', "''")}'`,
-        "ESCAPE": `'${escape.replace('\'', "''")}'`,
-      };
-      mimeType = 'text/csv';
-      fileExtension = 'csv';
-      break;
-    case 'exportJson':
-      compression = byId('exportJsonCompression').value;
-      dateFormat = byId(tabName + 'DateFormat').value;
-      timestampFormat = byId(tabName + 'TimestampFormat').value;
-      rowDelimiter = byId(tabName + 'RowDelimiter').value;
-      copyStatementOptions = {
-        "FORMAT": 'json',
-        "DATEFORMAT": `'${dateFormat.replace('\'', "''")}'`,
-        "TIMESTAMPFORMAT": `'${timestampFormat.replace('\'', "''")}'`,
-        "COMPRESSION": `'${compression.replace('\'', "''")}'`,
-        "ARRAY": rowDelimiter.toUpperCase()
-      };
-      mimeType = 'application/json';
-      fileExtension = 'json';
-      break;
-    case 'exportParquet':
-      compression = byId('exportParquetCompression').value;
-      copyStatementOptions = {
-        "COMPRESSION": `'${compression.replace('\'', "''")}'`
-      };
-      mimeType = 'application/vnd.apache.parquet';
-      fileExtension = 'parquet';
-      break;
-    case 'exportSql':
-      mimeType = 'text/plain';
-      data = sql;
-      fileExtension = 'sql';
-      break;
-  }
-  var fileName = [title, fileExtension].join('.');
+    var sql, structure;
+    if (byId('exportResultShapePivot').checked){
+      structure = 'pivot';
+      sql = getDuckDbPivotSqlStatementForQueryModel(queryModel);
+    }
+    else
+    if (byId('exportResultShapeTable').checked){
+      structure = 'table';
+      sql = getDuckDbTableSqlStatementForQueryModel(queryModel);
+    }
+      
+    var selectedTab = TabUi.getSelectedTab('#exportDialog');
+    var tabName = selectedTab.getAttribute('for');
+    
+    var mimeType, compression, includeHeaders,
+        dateFormat, timestampFormat, nullValueString,
+        columnDelimiter, quote, escape, rowDelimiter
+    ;
+    var fileExtension, data, copyStatementOptions;
+    switch (tabName) {
+      case 'exportDelimited':
+        columnDelimiter = byId(tabName + 'ColumnDelimiter').value;
+        nullValueString = byId(tabName + 'NullString').value;
+        includeHeaders = byId(tabName + 'IncludeHeaders').checked;
+        quote = byId(tabName + 'Quote').value;
+        escape = byId(tabName + 'Escape').value;
+        dateFormat = byId(tabName + 'DateFormat').value;
+        timestampFormat = byId(tabName + 'TimestampFormat').value;
+        compression = byId(tabName + 'Compression').value;
+        copyStatementOptions = {
+          "FORMAT": 'csv',
+          "DELIMITER": `'${columnDelimiter.replace('\'', "''")}'`,
+          "NULL": `'${nullValueString.replace('\'', "''")}'`,
+          "HEADER": includeHeaders ? 'TRUE' : 'FALSE',
+          "QUOTE": `'${quote.replace('\'', "''")}'`,
+          "ESCAPE": `'${escape.replace('\'', "''")}'`,
+          "DATEFORMAT": `'${dateFormat.replace('\'', "''")}'`,
+          "TIMESTAMPFORMAT": `'${timestampFormat.replace('\'', "''")}'`,
+          "COMPRESSION": `'${compression.replace('\'', "''")}'`,
+          "ESCAPE": `'${escape.replace('\'', "''")}'`,
+        };
+        mimeType = 'text/csv';
+        fileExtension = 'csv';
+        break;
+      case 'exportJson':
+        compression = byId(tabName + 'Compression').value;
+        dateFormat = byId(tabName + 'DateFormat').value;
+        timestampFormat = byId(tabName + 'TimestampFormat').value;
+        rowDelimiter = byId(tabName + 'RowDelimiter').value;
+        copyStatementOptions = {
+          "FORMAT": 'json',
+          "DATEFORMAT": `'${dateFormat.replace('\'', "''")}'`,
+          "TIMESTAMPFORMAT": `'${timestampFormat.replace('\'', "''")}'`,
+          "COMPRESSION": `'${compression.replace('\'', "''")}'`,
+          "ARRAY": rowDelimiter.toUpperCase()
+        };
+        mimeType = 'application/json';
+        fileExtension = 'json';
+        break;
+      case 'exportParquet':
+        compression = byId(tabName + 'Compression').value;
+        copyStatementOptions = {
+          "COMPRESSION": `'${compression.replace('\'', "''")}'`
+        };
+        mimeType = 'application/vnd.apache.parquet';
+        fileExtension = 'parquet';
+        break;
+      case 'exportSql':
+        mimeType = 'text/plain';
+        data = sql;
+        fileExtension = 'sql';
+        break;
+    }
 
-  if (copyStatementOptions){
-    var tmpFile = [crypto.randomUUID(), fileExtension].join('.');
-    var copyStatement = getCopyToStatement(sql, tmpFile, copyStatementOptions);
-    var datasource = queryModel.getDatasource();
-    var connection, result;
-    try {
-      connection = datasource.createManagedConnection();
-      result = await connection.query(copyStatement);
-      data = await connection.copyFileToBuffer(tmpFile);
-    }
-    catch (e){
-      showErrorDialog(e);
-      return;
-    }
-    finally {
-      if (result){
-        await result.close();
-      }
-      if (data) {
-        await connection.dropFile(fileName);
-      }
-      if (connection){
-        connection.destroy()
-      }
+    if (compression !== 'UNCOMPRESSED'){
+      fileExtension += '.' + compression.toLowerCase();
     }
     
-  }
-  
-  var destination;
-  if (byId('exportDestinationFile').checked){
-    destination = 'file';
-  }
-  else
-  if (byId('exportDestinationClipboard').checked){
-    destination = 'clipboard';
-  }
-  switch (destination){
-    case 'file':
-      downloadBlob(data, fileName, mimeType)
-      break;
-    case 'clipboard':
-      var text;
-      if (typeof data === 'string'){
-        text = data;
+    if (copyStatementOptions){
+      var tmpFileName = [crypto.randomUUID(), fileExtension].join('.');
+      progressMessageElement.innerText = `Preparing copy to ${tmpFileName}`;
+      var copyStatement = getCopyToStatement(sql, tmpFileName, copyStatementOptions);
+      var datasource = queryModel.getDatasource();
+      var connection, result;
+      try {
+        connection = datasource.createManagedConnection();
+        result = await connection.query(copyStatement);
+        progressMessageElement.innerText = `Extracting from ${tmpFileName}`;
+        data = await connection.copyFileToBuffer(tmpFileName);
       }
-      else {
-        text = new TextDecoder('utf-8').decode(data);
+      finally {
+        if (data) {
+          await connection.dropFile(tmpFileName);
+        }
+        if (connection){
+          connection.destroy();
+        }
       }
-      copyToClipboard(text, 'text/plain')
-      .then(function(){
-        debugger;
-      })
-      .catch(function(e){
-        showErrorDialog(e);
-      });
-      break;
+      
+    }
+    
+    var destination;
+    if (byId('exportDestinationFile').checked){
+      destination = 'file';
+    }
+    else
+    if (byId('exportDestinationClipboard').checked){
+      destination = 'clipboard';
+    }
+    
+    switch (destination){
+      case 'file':
+        var fileName = [title, fileExtension].join('.');      
+        progressMessageElement.innerText = `Download as ${fileName}`;
+        downloadBlob(data, fileName, mimeType);
+        break;
+      case 'clipboard':
+        var text;
+        if (typeof data === 'string'){
+          text = data;
+        }
+        else {
+          progressMessageElement.innerText = `Copying to clipboard..`;
+          text = new TextDecoder('utf-8').decode(data);
+        }
+        await copyToClipboard(text, 'text/plain');
+        break;
+    }
+    progressMessageElement.innerText = `Success!`;
   }
-  
+  catch (e){
+    progressMessageElement.innerText = `Error!`;
+    showErrorDialog(e);
+  }
+  finally {
+    dom.setAttribute('aria-busy', String(false));
+  }
 }
 
 function initExportUi(){
