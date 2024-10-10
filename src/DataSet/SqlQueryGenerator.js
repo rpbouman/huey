37dcdf48d193a2f610d6edad1a876a30f61e90db
Roland Bouman <Roland.Bouman@gmail.com>
2024-10-10 22:33:04 +0200

    initial implementation

--- src/DataSet/SqlQueryGenerator.js
+++ src/DataSet/SqlQueryGenerator.js
@@ -1,41 +1,6 @@
-class TupleSet extends DataSetComponent {
+class SqlQueryGenerator {
 
-  static groupingIdAlias = '__huey_grouping_id';
-   
-  static getSqlSelectExpressions(queryModel, axisId, includeCountAll){
-    var queryAxis = queryModel.getQueryAxis(axisId);
-    var queryAxisItems = queryAxis.getItems();
-    if (!queryAxisItems.length) {
-      return undefined;
-    }
-    
-    var selectListExpressions = {};
-    for (var i = 0; i < queryAxisItems.length; i++) {
-      var queryAxisItem = queryAxisItems[i];
-      var caption = QueryAxisItem.getCaptionForQueryAxisItem(queryAxisItem);
-      var selectListExpression = QueryAxisItem.getSqlForQueryAxisItem(queryAxisItem);
-      selectListExpressions[caption] = selectListExpression;
-    }
-    
-    if (includeCountAll) {
-      var countExpression = 'COUNT(*) OVER ()';
-      selectListExpressions[countExpression] = countExpression;
-    }
-    return selectListExpressions;
-  }
-  
-  static getSqlSelectStatementXXX(queryModel, axisId, includeCountAll) {
-    var sqlOptions = normalizeSqlOptions();
-    var queryAxis = queryModel.getQueryAxis(axisId);
-    var queryAxisItems = queryAxis.getItems();
-    if (!queryAxisItems.length) {
-      return undefined;
-    }
-    
-    var dataFoundationAlias = '__huey_data_foundation';
-    var datasource = queryModel.getDatasource();
-    var fromClause = datasource.getFromClauseSql(dataFoundationAlias);
-        
+  static #getUnnestingFunctions(){
     var unnestingFunctions = {};
     Object.keys(AttributeUi.arrayDerivations).forEach(function(arrayDerivationKey){
       var arrayDerivation = AttributeUi.arrayDerivations[arrayDerivationKey];
@@ -45,11 +10,14 @@ class TupleSet extends DataSetComponent {
       }
       unnestingFunctions[unnestingFunction + '()'] = arrayDerivation;
     });
+    return unnestingFunctions;
+  }
 
+  static #getFilterItemsByNestingStage(filterAxisItems){
     // analyze the filter items and store them in a dict: 
     // - filter items that require unnesting are stored by memberExpressionPath
-    var filterAxis = queryModel.getFiltersAxis();
-    var filterAxisItems = filterAxis.getItems();
+    var unnestingFunctions = SqlQueryGenerator.#getUnnestingFunctions();
+    
     var filterAxisItemsByNestingStage = {};
     _filterAxisIems: for (var i = 0; i < filterAxisItems.length; i++){
       var filterAxisItem = filterAxisItems[i];
@@ -76,16 +44,28 @@ class TupleSet extends DataSetComponent {
         filterAxisItemsByNestingStage[memberExpressionPathString] = filterAxisItemsForStage;
       }
       filterAxisItemsForStage.push(filterAxisItem);
-    }      
+    }
+    return filterAxisItemsByNestingStage;
+  }
+  
+  static #getConditionForFilterItems(filterItems, tableAlias){
+    return filterItems
+    .map(function(filterItem){
+      return QueryAxisItem.getFilterConditionSql(filterItem, tableAlias);
+    })
+    .join('\nAND ');
+  }
+  
+  static #getUnnestingStages(datasource, queryAxisItems, filterAxisItemsByNestingStage){
+    var unnestingFunctions = SqlQueryGenerator.#getUnnestingFunctions();    
+    var dataFoundationAlias = '__huey_data_foundation';
+    var fromClause = datasource.getFromClauseSql(dataFoundationAlias);
     
-    var filterConditions, whereClause = undefined;
-    
-    filterAxisItems = filterAxisItemsByNestingStage[''];
+    var whereCondition = undefined;
+    var filterAxisItems = filterAxisItemsByNestingStage[''];
     if (filterAxisItems && filterAxisItems.length) {
-      filterConditions = filterAxisItems.map(function(filterAxisItem){
-        return QueryAxisItem.getFilterConditionSql(filterAxisItem, dataFoundationAlias);
-      });      
-      whereClause = filterConditions.join('\nAND ');
+      whereCondition = SqlQueryGenerator.#getConditionForFilterItems(filterAxisItems, dataFoundationAlias);
+      delete filterAxisItemsByNestingStage[''];
     }
     
     var cteIndex = 0;
@@ -93,7 +73,7 @@ class TupleSet extends DataSetComponent {
     var cte, ctes = [{
       items: currentItems,
       from: fromClause,
-      where: whereClause,
+      where: whereCondition,
       alias: dataFoundationAlias,
     }];
     
@@ -191,9 +171,10 @@ class TupleSet extends DataSetComponent {
       }
       // if we didn't find a new unnesting stage, we are done.
     } while(unnestingItem);
-
-    
-    var cte = ctes.pop();
+    return ctes;
+  }
+  
+  static #getSqlSelectStatementForFinalStage(cte){
     var sql = '';
     var columnExpressions = {};
     cte.items.forEach(function(item, index){
@@ -290,8 +271,35 @@ class TupleSet extends DataSetComponent {
     sql.push(groupByClause);
     sql.push(`ORDER BY ${orderByExpressions.join('\n,')}`);
     sql = sql.join('\n');
-        
+    return sql;
+  }
+
+  static getSqlSelectStatement(queryModel, axisId, includeCountAll) {
+    var sqlOptions = normalizeSqlOptions();
+    var queryAxis = queryModel.getQueryAxis(axisId);
+    var queryAxisItems = queryAxis.getItems();
+    if (!queryAxisItems.length) {
+      return undefined;
+    }
     
+    var datasource = queryModel.getDatasource();
+        
+    var unnestingFunctions = SqlQueryGenerator.#getUnnestingFunctions();
+
+    // analyze the filter items and store them in a dict: 
+    // - filter items that require unnesting are stored by memberExpressionPath
+    var filterAxis = queryModel.getFiltersAxis();
+    var filterAxisItems = filterAxis.getItems();
+    
+    var filterAxisItemsByNestingStage = SqlQueryGenerator.#getFilterItemsByNestingStage(filterAxisItems);    
+
+    var ctes = SqlQueryGenerator.#getUnnestingStages(
+      datasource,
+      queryAxisItems, 
+      filterAxisItemsByNestingStage
+    );
+    
+    var sql = SqlQueryGenerator.#getSqlSelectStatementForFinalStage(ctes.pop()); 
     var sqls = ctes.map(function(cte){
       var sql;
       var selectListExpressions = {};
@@ -332,222 +340,6 @@ class TupleSet extends DataSetComponent {
     
     return sql;
   }
-  
-  static getSqlSelectStatement(queryModel, axisId, includeCountAll){
-    var sql = SqlQueryGenerator.getSqlSelectStatement(queryModel, axisId, includeCountAll);
-    return sql;
-  }
-         
-  #queryAxisId = undefined;
 
-  #tuples = [];
-  #tupleValueFields = [];
-  
-  #tupleCount = undefined;
-  #pageSize = 50;
-  
-  constructor(queryModel, axisId){
-    super(queryModel);
-    this.#queryAxisId = axisId;  
-  }
 
-  getTupleValueFields(){
-    return this.#tupleValueFields;
-  }
-  
-  getPageSize(){
-    return this.#pageSize;
-  }
-
-  setPageSize(pageSize){
-    this.#pageSize = pageSize;
-  }
-  
-  getQueryAxisId(){
-    return this.#queryAxisId;
-  }
-
-  getQueryAxisItems(){
-    var queryModel = this.getQueryModel();
-    var axisId = this.#queryAxisId;
-    
-    var queryAxis = queryModel.getQueryAxis(axisId);
-    var items = queryAxis.getItems();
-    return items;
-  }
-
-  #getSqlSelectStatement(includeCountAll, values){
-    var queryModel = this.getQueryModel();
-    if (!queryModel) {
-      return undefined;
-    }
-    var sql = TupleSet.getSqlSelectStatement(
-      queryModel, 
-      this.#queryAxisId, 
-      includeCountAll, 
-      values
-    );
-    return sql;
-  }
-  
-  clear(){
-    this.#tuples = [];
-    this.#tupleCount = undefined;
-  }
-  
-  getTupleCountSync() {
-    return this.#tupleCount;
-  }
-  
-  getTuplesSync(from, to){
-    return this.#tuples.slice(from, to);
-  }
-  
-  getTupleSync(index){
-    return this.#tuples[index];
-  }
-    
-  async getTupleCount(){
-    if (this.#tupleCount === undefined) {
-      
-    }
-    return new Promise(function(resolve, reject){
-      resolve(this.#tupleCount);
-    });
-  }
-
-  #loadTuples(resultSet, offset) {
-    var numRows = resultSet.numRows;
-    
-    var fields = resultSet.schema.fields;
-    
-    var items = this.getQueryAxisItems();
-    var hasGroupingId = false, fieldOffset = 0, fieldCount = items.length;
-    if (fields[0].name === TupleSet.groupingIdAlias) {
-      hasGroupingId = true;
-      fieldOffset += 1;
-      fieldCount += 1;
-    }
-
-    var tuples = this.#tuples;
-
-    // if the offset is 0 we should have included an expression that computes the total count as last
-    if (offset === 0) {
-      if (numRows === 0){
-        this.#tupleCount = 0;
-      }
-      else {
-        var firstRow = resultSet.get(0);
-        var lastField = fields[fields.length - 1];
-        var totalCount = firstRow[lastField.name];
-        this.#tupleCount = parseInt(String(totalCount), 10);
-      }
-    }
-    this.#tupleValueFields = fields.slice(fieldOffset, fieldCount);
-        
-    for (var i = 0; i < numRows; i++){
-
-      var row = resultSet.get(i);
-      var values = [];
-      var tuple = {values: values};
-
-      if (hasGroupingId){
-        var groupingId = row[TupleSet.groupingIdAlias];
-        if (groupingId > 0) {
-          tuple[TupleSet.groupingIdAlias] = groupingId;
-        }
-      }
-
-      for (var j = fieldOffset; j < fieldCount; j++){
-        var field = fields[j];
-        var fieldName = field.name;
-        var value = row[fieldName];
-        values[j - fieldOffset] = value;
-      }
-
-      tuples[offset + i] = tuple;      
-    }
-  }
-
-  async #executeAxisQuery(limit, offset){
-    var includeCountExpression = offset === 0;
-
-    var axisSql = this.#getSqlSelectStatement(includeCountExpression);
-    if (!axisSql){
-      return 0;
-    }
-    axisSql = `${axisSql}\nLIMIT ${limit} OFFSET ${offset}`;
-
-    var connection = await this.getManagedConnection();
-    console.log(`SQL to fetch tuples for ${this.#queryAxisId} axis:`);
-    var resultset = await connection.query(axisSql);
-    console.log(`Query method returned, connection ${connection.getConnectionId()} in state ${connection.getState()}` );
-    var rejects = await this.getQueryModel().getDatasource().getRejects();
-    if (connection.getState() === 'canceled') {
-      return 0;
-    }
-    this.#loadTuples(resultset, offset);
-
-    return resultset.numRows;
-  }
-
-  getCachedTupleCount(offset){
-    var data = this.#tuples;
-    var cachedTupleCount = 0;
-    for (var i = offset; i < this.#tupleCount; i++){
-      var tuple = data[i];
-      if (!tuple){
-        break;
-      }
-      cachedTupleCount += 1;
-    }
-    return cachedTupleCount;
-  }
-
-  async getTuples(count, offset){
-    
-    var data = this.#tuples;
-    var tuples = [];
-
-    var i = 0;
-    var firstIndexToFetch, lastIndexToFetch;
-
-    if (this.#tupleCount !== undefined && offset + count > this.#tupleCount) {
-      count = this.#tupleCount - offset;
-    }
-    
-    while (i < count) {
-      var tupleIndex = offset + i;
-      var tuple = data[tupleIndex];
-      if (tuple === undefined) {
-        if (firstIndexToFetch === undefined) {
-          firstIndexToFetch = tupleIndex;
-          lastIndexToFetch = tupleIndex;
-        }
-        else 
-        if (tupleIndex > lastIndexToFetch) {
-          lastIndexToFetch = tupleIndex;
-        }
-      }
-      else {
-        tuples[i] = tuple;
-      }
-      i += 1;
-    }
-    
-    if (firstIndexToFetch === undefined) {
-      return tuples;
-    }
-    
-    lastIndexToFetch += 1;
-    var newCount = (lastIndexToFetch - firstIndexToFetch);
-    if (newCount < this.#pageSize && (offset + count === lastIndexToFetch) && lastIndexToFetch < this.#tupleCount) {
-      newCount = this.#pageSize;
-    }
-    
-    var numRows = await this.#executeAxisQuery(newCount, firstIndexToFetch);
-    tuples = data.slice(offset, offset + count);
-    return tuples;
-  }    
-  
 }
