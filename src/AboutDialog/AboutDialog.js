1e8e4cfc4f5a9b0273e6a4becf24d956ccaa6c6b
Roland Bouman <Roland.Bouman@gmail.com>
2024-11-13 12:35:15 +0100

    Load tabler icon font dynamically.

--- src/AboutDialog/AboutDialog.js
+++ src/AboutDialog/AboutDialog.js
@@ -1,7 +1,15 @@
 function initAboutDialog(){
-  var link = byId('duckDbLibraryUrl');
-  link.setAttribute('href', duckDbLibraryUrl);
-  link.innerText = `DuckDB WASM ${duckdbLibraryVersion}`;
+  var el;
+
+  el = byId('hueyVersion');
+  el.innerText = `Huey version ${hueyVersionNumber} - ${hueyVersionName}`;
+
+  el = byId('tablerIconsUrl');
+  el.innerText = `Tabler Icons v${tablerIconsFontVersion}`;
+  
+  el = byId('duckDbLibraryUrl');
+  el.setAttribute('href', duckDbLibraryUrl);
+  el.innerText = `DuckDB WASM ${duckdbLibraryVersion}`;
 }
 
 initAboutDialog();
