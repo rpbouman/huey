d2425b7a2bc2014720a42f2a118571db1bd34711
Roland Bouman <Roland.Bouman@gmail.com>
2025-09-03 22:30:55 +0200

    Initial implementation of https://github.com/rpbouman/huey/issues/581
    - add new translations files in src/Internationalization/i18n
    - internationalization files are json files, having the locale as name, and js as extension
    - internationalization files contain a single call to Internationalization.setTexts()
    - pass a object to Internationalization.setTexts() with the english language label as key, and the translation as value
    - To create a new translation, you can use src/Internationalization/huey.i18n.labels.template.js as starting point.
    - To keep the template up to date, you can call Internationalization.getTextsTemplate(), which returns an empty template.

---
+++ src/Internationalization/Internationalization.js
@@ -0,0 +1,255 @@
+class Internationalization {
+  
+  static #texts = {};
+  static #currentLanguage = undefined;
+  static #languageIndex = undefined;
+  static #hueyNativeLanguage = 'en';
+  
+  static {
+    document.addEventListener('DOMContentLoaded', Internationalization.#init);
+    window.addEventListener('languagechange', Internationalization.#languageChanged)
+  }
+    
+  static #init(event){
+    var languages = navigator.languages;
+    Internationalization.#loadTexts();
+  }
+  
+  static #languageChanged(event){
+    console.log('Language change occurred, re-initializing Internationalization.');
+    Internationalization.#languageIndex = undefined;
+    Internationalization.#texts = {};
+    Internationalization.#init();
+  }
+  
+  static #getScriptElement(){
+    var elementName = 'script';
+    var elementId = 'InterationalizationTexts';
+    var scripElement = document.querySelector(`${elementName}#${elementId}`);
+    if (!scripElement){
+      scripElement = document.createElement(elementName);
+      scripElement.setAttribute('id', elementId);
+      document.body.appendChild(scripElement);
+      scripElement.addEventListener('load', Internationalization.textsLoaded);
+    }
+    return scripElement;
+  }
+  
+  static #loadTexts(){
+    if (Internationalization.#languageIndex === undefined) {
+      Internationalization.#languageIndex = 0;
+    }
+    var languages = navigator.languages;
+    if (Internationalization.#languageIndex >= languages.length){
+      console.log(`Internationalization: No more languages to attempt.`);
+    }
+    var language = languages[ Internationalization.#languageIndex++ ];
+    if (language === Internationalization.#hueyNativeLanguage){
+      console.log(`No need to load Internationalization texts for Huey native language "${Internationalization.#hueyNativeLanguage}".`);
+      Internationalization.#applyTexts(true);
+      return;
+    }
+    
+    var url = `Internationalization/i18n/${language}.js`;
+    console.log(`Attempt to load Internationzalization texts from "${url}"`);    
+    var scriptElement = Internationalization.#getScriptElement();
+    scriptElement.src = url;
+  }
+  
+  static textsLoaded(event){
+    var scriptElement = event.target;
+    var message = `Attempt to load Internationzalization texts from "${scriptElement.src}"`;
+    if (Internationalization.#texts === undefined){
+      console.log(message + ' failed.');
+      Internationalization.#loadTexts();
+    }
+    else {
+      var src = scriptElement.src;
+      var parts = src.split('/');
+      var resource = parts.pop();
+      parts = resource.split('.');
+      var language = parts[0];
+      Internationalization.#currentLanguage = language;
+      console.log(message + ' succeeded.');
+      Internationalization.#applyTexts();
+    }
+  }
+  
+  static setTexts(texts){
+    console.log('setTexts');
+    var textObject;
+    switch (typeof(texts)){
+      case 'object':
+        // stringify as safety measure
+        texts = JSON.stringify(texts);
+        textObject = texts;
+      case 'string':
+        textObject = JSON.parse(texts);
+    }
+    Internationalization.#texts = textObject;
+    Internationalization.#applyTexts();
+  }
+  
+  static #getTextContentElements(){
+    var textContentElements = document.querySelectorAll('*:not( .i18nIgnore ):not( :is(style, script, link) ):not( :has( > * ) ):not( :empty )');
+    return textContentElements;
+  }
+  
+  static #getAttributeElements(){
+    var attributeNames = [
+      'alt',
+      'placeholder',
+      'title', 
+      'aria-braillelabel',
+      'aria-brailleroledescription',
+      'aria-description',
+      'aria-label', 
+      'aria-placeholder',
+      'aria-roledescription',
+      'aria-valuenow',
+      'aria-valuetext'
+    ];
+    
+    var selection = {};
+    for (var i = 0; i < attributeNames.length; i++){
+      var attributeName = attributeNames[i];
+      var elements = document.querySelectorAll(`*:not( .i18nIgnore )[${attributeName}]`);
+      selection[attributeName] = elements;
+    }
+    return selection;
+  }
+  
+  static #visitAllTexts(callback){
+    var textContentElements = Internationalization.#getTextContentElements();
+    var i18nNativeAttributeName = 'data-i18n-native-text';
+    for (var i = 0; i < textContentElements.length; i++){
+      var element = textContentElements.item(i);
+      var key, value;
+
+      if (element.hasAttribute(i18nNativeAttributeName)){
+        key = element.getAttribute(i18nNativeAttributeName);
+      }
+      else {
+        key = element.textContent.trim();
+        if (key.length === 0){
+          continue;
+        }
+      }
+      value = Internationalization.getText(key);
+            
+      callback({
+        element: element,
+        i18nNativeAttributeName: i18nNativeAttributeName,
+        key: key,
+        value: value
+      });      
+    }
+      
+    var attributeElements = Internationalization.#getAttributeElements();
+    for (var attributeName in attributeElements){
+      var elements = attributeElements[attributeName];
+      var key, value;
+      
+      for (var j = 0; j < elements.length; j++){
+        var element = elements.item(j);
+        if (!element.hasAttribute(attributeName)){
+          continue;
+        }
+        
+        var i18nNativeAttributeName = `data-i18n-native-${attributeName}`;
+        if (element.hasAttribute(i18nNativeAttributeName)){
+          key = element.getAttribute(i18nNativeAttributeName);
+        }
+        else {
+          key = element.getAttribute(attributeName).trim();
+          if (key.length === 0){
+            continue;
+          }
+        }
+        value = Internationalization.getText(key);
+        callback({
+          element: element,
+          i18nNativeAttributeName: i18nNativeAttributeName,
+          attributeName: attributeName,
+          key: key,
+          value: value
+        });      
+      }
+    }
+  }
+  
+  static #applyTexts(toNative){
+    Internationalization.#visitAllTexts(function(object){
+      var element = object.element;
+      var key = object.key;
+      var value = object.value;
+      
+      var text = toNative === true ? key : value || object.key;
+      var attributeName = object.attributeName;
+      if (attributeName){
+        element.setAttribute(attributeName, text);
+      }
+      else {
+        element.textContent = text;
+      }
+      
+      var i18nNativeAttributeName = object.i18nNativeAttributeName;
+      if (!element.hasAttribute(i18nNativeAttributeName)){
+        element.setAttribute(i18nNativeAttributeName, key);
+      }
+    });
+  }
+    
+  static getTextsTemplate(){
+    var allTexts = {};
+    
+    Internationalization.#visitAllTexts(function(object){
+      var element = object.element;
+      var key = object.key;
+      var value = object.value;
+      allTexts[key] = value || null;
+    });
+    
+    return Object.keys(allTexts).sort(function(a, b){
+      var A = a.toUpperCase();
+      var B = b.toUpperCase();
+      if (A > B) {
+        return 1;
+      }
+      else
+      if (A < B){
+        return -1;
+      }
+      if (A > B) {
+        return 1;
+      }
+      else
+      if (a > b){
+        return 1;
+      }
+      else
+      if (a < b){
+        return -1;
+      }
+      return 0;
+    }).reduce(function(acc, curr){
+      acc[curr] = allTexts[curr];
+      return acc;
+    },{});
+  }
+  
+  static getText(key){
+    var text = Internationalization.#texts[key];
+    if (text === undefined){
+      console.warn(`Translation for content "${key}" not found`);
+      return undefined;
+    }
+    var args = arguments;
+    return text.replace(/\{\d+\}/g, function(match){
+      var index = match.slice(1, -1);
+      index = parseInt(index, 10);
+      return args[index];
+    })
+  }
+  
+}
