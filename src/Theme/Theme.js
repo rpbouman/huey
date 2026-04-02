6195ff9117b7323074de8756e7ee49e755d6d28a
Roland Bouman <Roland.Bouman@gmail.com>
2026-04-02 23:05:22 +0200

    remove commented out code.

--- src/Theme/Theme.js
+++ src/Theme/Theme.js
@@ -65,39 +65,5 @@ settings.addEventListener('change', function(event){
   Theme.applyTheme( themes.selectedIndex );  
 });
 
-/*
-Theme editor - put it on ice for now.
-
-              <!-- getComputedStyle(document.body).getPropertyValue('--huey-medium-background-color') -->
-              <label for="textFontFamily">Text Font</label>
-              <input type="text" id="textFontFamily" value="Verdana" onchange="updateCssVariable(this)"/>
-              <label for="monoFontFamily">Monospace Font</label>
-              <input type="text" id="monoFontFamily" value="Monospace" onchange="updateCssVariable(this)"/>
-
-              <label for="foregroundColor">Foreground Color</label>
-              <input type="color" id="foregroundColor" data-css-var="huey-foreground-color" onchange="updateCssVariable(this)"/>
-              <label for="placeholderColor">Placeholder Color</label>
-              <input type="color" id="placeholderColor" data-css-var="huey-placeholder-color" onchange="updateCssVariable(this)"/>
-              
-              <label for="lightBackgroundColor">Light Background Color</label>
-              <input type="color" id="lightBackgroundColor" data-css-var="huey-icon-color-subtle" onchange="updateCssVariable(this)"/>
-              <label for="mediumBackgroundColor">Medium Background Color</label>
-              <input type="color" id="mediumBackgroundColor" onchange="updateCssVariable(this)"/>
-              <label for="darkBackgroundColor">Dark Background Color</label>
-              <input type="color" id="darkBackgroundColor" onchange="updateCssVariable(this)"/>
-
-              <label for="lightBorderColor">Light Border Color</label>
-              <input type="color" id="lightBorderColor" onchange="updateCssVariable(this)"/>
-              <label for="darkBorderColor">Dark Border Color</label>
-              <input type="color" id="darkBorderColor" onchange="updateCssVariable(this)"/>
-
-              <label for="iconColor">Icon Color</label>
-              <input type="color" id="iconColor" onchange="updateCssVariable(this)"/>
-              <label for="iconColorSubtle">Icon Color Subtle</label>
-              <input type="color" id="iconColorSubtle" onchange="updateCssVariable(this)"/>
-              <label for="iconColorHighlight">Icon Color Highlight</label>
-              <input type="color" id="iconColorHighlight" onchange="updateCssVariable(this)"/>
-
-*/
 
 
