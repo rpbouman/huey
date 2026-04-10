accbc8771ba905e574ca5bbb7b292bcd06a124b9
Roland Bouman <Roland.Bouman@gmail.com>
2026-04-10 03:58:03 +0200

    Modernize js

--- src/DataSource/duckdb/DuckDbConnection.js
+++ src/DataSource/duckdb/DuckDbConnection.js
@@ -1,9 +1,9 @@
 class DuckDbConnection extends EventEmitter {
-  
+
   #duckDbInstance = undefined;
   #physicalConnection = undefined;
   #state = 'unconnected';
-  
+
   constructor(duckDbInstance) {
     super(['beforequery','afterquery']);
     this.#duckDbInstance = duckDbInstance;
@@ -17,44 +17,44 @@ class DuckDbConnection extends EventEmitter {
     }
     return this.#physicalConnection;
   }
-  
+
   async prepareStatement(sql){
-    var connection = await this.getPhysicalConnection();
+    const connection = await this.getPhysicalConnection();
     this.#state = 'preparing';
-    var preparedStatement = await connection.prepare(sql);
+    const preparedStatement = await connection.prepare(sql);
     this.#state = 'prepared';
     return preparedStatement;
   }
-  
+
   getConnectionId(){
     if (this.#physicalConnection === undefined){
       return undefined;
     }
     return this.#physicalConnection._conn;
   }
-  
+
   async query(sql){
-    var connection = await this.getPhysicalConnection();
-    
+    const connection = await this.getPhysicalConnection();
+
     // TODO: allow query to be canceled?
     this.fireEvent('beforequery', {
       physicalConnection: connection,
       sql: sql
     });
-    
+
     this.#state = 'querying';
-    var msg = `Executing ${sql} on connection ${this.getConnectionId()}`;
+    const msg = `Executing ${sql} on connection ${this.getConnectionId()}`;
     console.time(msg);
-    var result = await connection.query(sql);
+    const result = await connection.query(sql);
     console.timeEnd(msg);
     this.#state = 'queried';
-    
+
     this.fireEvent('afterquery', {
       physicalConnection: connection,
       sql: sql,
       result: result
     });
-    
+
     return result;
   }
 
@@ -65,7 +65,7 @@ class DuckDbConnection extends EventEmitter {
     console.log(`canceling pending queries for ${this.getConnectionId()}, current state: ${this.#state}.`);
     this.#state = 'canceling';
     try {
-      var canceled = await this.#duckDbInstance.cancelPendingQuery(this.#physicalConnection);
+      const canceled = await this.#duckDbInstance.cancelPendingQuery(this.#physicalConnection);
       console.log(`canceled pending queries for ${this.getConnectionId()}, result: ${canceled}.`);
       if (canceled) {
         this.#state = 'canceled';
@@ -82,32 +82,32 @@ class DuckDbConnection extends EventEmitter {
     }
     return this.#state;
   }
-  
+
   registerFile(file, protocol){
     if (! (file instanceof File)){
       throw new Error(`Invalid argument! Need instance of File.`);
     }
     protocol = protocol || window.hueyDb.duckDb.DuckDBDataProtocol.BROWSER_FILEREADER;
     return this.#duckDbInstance.registerFileHandle(
-      file.name, 
-      file, 
+      file.name,
+      file,
       protocol
     );
   }
- 
+
   copyFileToBuffer(fileName){
     return this.#duckDbInstance.copyFileToBuffer(fileName);
   }
-  
+
   dropFile(fileName){
     return this.#duckDbInstance.dropFile(fileName);
   }
-  
+
   async close(){
     if (this.#physicalConnection){
       try {
-        this.#state = 'closing';        
-        var result = await this.#physicalConnection.close();
+        this.#state = 'closing';
+        const result = await this.#physicalConnection.close();
         this.#state = 'closed';
         return result;
       }
@@ -124,7 +124,7 @@ class DuckDbConnection extends EventEmitter {
       return null;
     }
   }
-  
+
   async destroy(){
     if (this.#physicalConnection){
       try {
@@ -137,7 +137,7 @@ class DuckDbConnection extends EventEmitter {
     this.#state = 'destroyed';
     this.#physicalConnection = null
   }
- 
+
   getState(){
     return this.#state;
   }
