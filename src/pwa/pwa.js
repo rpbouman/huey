4870855e9205f5eab25df6b89395616ed79a0a3f
Roland Bouman <Roland.Bouman@gmail.com>
2026-04-13 12:13:01 +0200

    forgot await.

--- src/pwa/pwa.js
+++ src/pwa/pwa.js
@@ -6,7 +6,7 @@ function initLaunchQueue(){
         return;
       }
       // unwrap file system handle (given by pwa) to plain old web api File objects
-      files = FileUtils.unwrapFileSystemHandleToFile( files );
+      files = await FileUtils.unwrapFileSystemHandleToFile( files );
       uploadResults = await uploadUi.uploadFiles(files);
       afterUploaded(uploadResults);
     });
