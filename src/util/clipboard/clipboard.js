21d0d6b461fc6ace57870cc2f697551011785300
Roland Bouman <Roland.Bouman@gmail.com>
2026-04-15 01:17:03 +0200

    fix

--- src/util/clipboard/clipboard.js
+++ src/util/clipboard/clipboard.js
@@ -5,7 +5,8 @@ function createClipboardItem(blob, mimeType){
 }
 
 async function copyToClipboard(data, mimeType) {
-  const clipboard = navigator.clipboard, method, arg;
+  const clipboard = navigator.clipboard
+  let method, arg;
   if (typeof data === 'string') {
     if (mimeType) {
       if (!ClipboardItem.supports(mimeType)){
