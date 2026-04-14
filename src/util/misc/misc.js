457bceb77d38e1f884f569888c17b0690c8e63b5
Roland Bouman <Roland.Bouman@gmail.com>
2026-04-14 22:22:00 +0200

    Modernize

--- src/util/misc/misc.js
+++ src/util/misc/misc.js
@@ -1,15 +1,19 @@
 function getCsv(data, options) {
   options = options || {};
-  var lineSeparator = options.lineSeparator || '\n';
-  var fieldSeparator = options.fieldSeparator || '\t';
-  var quoteChar = options.quoteChar || '"';
-  var escapeChar = options.escapeChar || quoteChar;
+  const lineSeparator = options.lineSeparator || '\n';
+  const fieldSeparator = options.fieldSeparator || '\t';
+  const quoteChar = options.quoteChar || '"';
+  const escapeChar = options.escapeChar || quoteChar;
   
-  return data.map(function(line){
-    return line.map(function(value){
+  return data.map(line => {
+    return line.map(value => {
       switch (typeof value) {
         case 'string':
-          if (value.indexOf(fieldSeparator) !== -1 || value.indexOf(lineSeparator) !== -1 || value.indexOf(quoteChar) !== -1){
+          if (
+            value.indexOf(fieldSeparator) !== -1 || 
+            value.indexOf(lineSeparator) !== -1 || 
+            value.indexOf(quoteChar) !== -1
+          ){
             value = value.replaceAll(escapeChar, escapeChar + escapeChar);
             if (quoteChar !== escapeChar) {
               value = value.replaceAll(quoteChar, escapeChar + quoteChar);
@@ -19,7 +23,6 @@ function getCsv(data, options) {
           break;
         default:
       }
-      
       return value;
     }).join(fieldSeparator);
   }).join(lineSeparator);
