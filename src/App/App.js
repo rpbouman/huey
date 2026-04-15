792292c8d9e57990a8e92e1e718c90732ecd4753
Roland Bouman <Roland.Bouman@gmail.com>
2026-04-15 11:03:56 +0200

    Modernize

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,30 @@
-const queryParams = document.location.search.substr(1).split('&').reduce(function(acc, nameValue){
-  nameValue = nameValue.split('=');
-  var name = nameValue[0];
-  var value = nameValue[1];
-  var existingValue = acc[name];
-  switch (typeof existingValue){
-    case 'undefined':
-      break;
-    case 'object':
-      existingValue.push(value);
-      value = existingValue;
-      break;
-    default:
-      value = [existingValue, value];
-  }
-  acc[name] = value;
-  return acc;
-}, {});
+const queryParams = document
+  .location
+  .search
+  .substr(1)
+  .split('&')
+  .reduce((acc, nameValue) => {
+    nameValue = nameValue.split('=');
+    const name = nameValue[0];
+    let value = nameValue[1];
+    const existingValue = acc[name];
+    switch (typeof existingValue){
+      case 'undefined':
+        break;
+      case 'object':
+        existingValue.push(value);
+        value = existingValue;
+        break;
+      default:
+        value = [existingValue, value];
+    }
+    acc[name] = value;
+    return acc;
+  }, {});
 
 function getDuckDbLogLevel(duckdb){
-  var loglevel;
-  var paramLoglevel = queryParams.loglevel;
+  let loglevel;
+  const paramLoglevel = queryParams.loglevel;
   if (paramLoglevel){
     loglevel = duckdb.LogLevel[paramLoglevel];
     if (typeof loglevel !== 'number'){
@@ -29,15 +34,16 @@ function getDuckDbLogLevel(duckdb){
   return typeof loglevel === 'number' ? loglevel : duckdb.LogLevel.INFO;
 }
 
+// unused. for convenience/debugging
 function duckDbRowToJSON(object){
-  var pojo;
+  let pojo;
   if (typeof object.toJSON === 'function'){
     pojo = object.toJSON();
   }
   else {
     pojo = object;
   }
-  return JSON.stringify(pojo, function(key, value){
+  return JSON.stringify(pojo, (key, value) => {
     if (value && value.constructor === BigInt){
       return parseFloat(value.toString());
     }
@@ -51,38 +57,38 @@ function initDuckdbVersion(){
   if (!window.hueyDb) {
     return;
   }
-  var connection = window.hueyDb.connection;
-  var versionColumn = 'version';
-  var apiColumn = 'api';
-  var reservedWordsColumn = 'reserved_words';
-  var columns = {
+  const connection = window.hueyDb.connection;
+  const versionColumn = 'version';
+  const apiColumn = 'api';
+  const reservedWordsColumn = 'reserved_words';
+  const columns = {
     "version()": versionColumn,
     "current_setting('duckdb_api')": apiColumn,
     "list( keyword_name )": reservedWordsColumn,
   };
-  var selectListSql = Object.keys(columns).map(function(key){
+  const selectListSql = Object.keys(columns).map(key => {
     return `${key} AS ${getQuotedIdentifier(columns[key])}`;
   }).join('\n,');
-  var sql = `SELECT ${selectListSql}`;
+  let sql = `SELECT ${selectListSql}`;
   sql += `\nFROM duckdb_keywords()\nWHERE keyword_category != 'unreserved'`;
-  var result = connection.query(sql)
-  .then(function(resultset){
-    var row = resultset.get(0);
-    var version = row[versionColumn];
-    var api = row[apiColumn];
-    var reservedWords = row[reservedWordsColumn];
-    reservedWords = String(reservedWords).slice(1, -1).split(',');
+  const result = connection.query(sql)
+  .then(resultset => {
+    const row = resultset.get(0);
+    const version = row[versionColumn];
+    const api = row[apiColumn];
+    const reservedWords = String( row[reservedWordsColumn] ).slice(1, -1).split(',');
     window.hueyDb.reservedWords = reservedWords;
 
-    var duckdbVersionLabel = byId('duckdbVersionLabel');
+    const duckdbVersionLabel = byId('duckdbVersionLabel');
     duckdbVersionLabel.textContent = `DuckDB ${version}, API: ${api}`;
     
-    var duckdbAvatar = byId('duckdb-version-specific-avatar');
-    var duckdbVersionParts = /v(\d+)\.(\d+).(\d)/.exec(version);
+    const duckdbAvatar = byId('duckdb-version-specific-avatar');
+    const duckdbVersionParts = /v(\d+)\.(\d+).(\d)/.exec(version);
     duckdbAvatar.src = `https://duckdb.org/images/release-icons/${duckdbVersionParts[1]}.${duckdbVersionParts[2]}.0.svg`
   })
-  .catch(function(){
+  .catch(error => {
     console.error(`Error fetching duckdb version info.`);
+    console.error(error);
   })
 }
 
@@ -95,9 +101,9 @@ async function analyzeDatasource(datasource){
   }
   catch (error) {
     attributeUi.clear(false);
-    var title = `Error reading datasource ${datasource.getId()}`;
+    const title = `Error reading datasource ${datasource.getId()}`;
     console.error(title);
-    var description = error.message;
+    const description = error.message;
     console.error(error);
     showErrorDialog({
       title: title,
@@ -108,16 +114,16 @@ async function analyzeDatasource(datasource){
 
 function initExecuteQuery(){
 
-  byId('runQueryButton').addEventListener('click', function(event){
+  byId('runQueryButton').addEventListener('click', event => {
     pivotTableUi.updatePivotTableUi();
   });
 
-  var autoRunQuery = byId('autoRunQuery');
-  var settingsPath = ['querySettings', 'autoRunQuery'];
+  const autoRunQuery = byId('autoRunQuery');
+  const settingsPath = ['querySettings', 'autoRunQuery'];
   autoRunQuery.checked = Boolean( settings.getSettings(settingsPath) );
-  autoRunQuery.addEventListener('change', function(event){
-    var target = event.target;
-    var checked = target.checked;
+  autoRunQuery.addEventListener('change', event => {
+    const target = event.target;
+    const checked = target.checked;
     settings.assignSettings('querySettings', {
       'autoRunQuery': checked
     });
@@ -147,60 +153,60 @@ function initApplication(){
   initDataSourceMenu();
   initPwa();
 
-  var currentRoute = Routing.getCurrentRoute();
+  const currentRoute = Routing.getCurrentRoute();
   if (currentRoute){
     pageStateManager.setPageState(currentRoute);
   }
 
-  bufferEvents(queryModel, 'change', function(event, count){
+  bufferEvents(queryModel, 'change', (event, count) => {
     if (count !== undefined) {
       return;
     }
 
     console.log(`buffered Events, event:`);
-    var eventData = event.eventData;
+    const eventData = event.eventData;
     console.log(eventData);
 
-    var currentDatasourceCaption, datasource = queryModel.getDatasource();
+    let currentDatasourceCaption, datasource = queryModel.getDatasource();
     if (datasource) {
       currentDatasourceCaption = DataSourcesUi.getCaptionForDatasource(datasource);
     }
     else {
       currentDatasourceCaption = '';
     }
-    byId('currentDatasource').setAttribute('data-current-datasource', currentDatasourceCaption);
-    byId('currentDatasource').firstChild.data = currentDatasourceCaption;
+    const currentDatasource = byId('currentDatasource');
+    currentDatasource.setAttribute('data-current-datasource', currentDatasourceCaption);
+    currentDatasource.firstChild.data = currentDatasourceCaption;
 
-    var title = ExportUi.generateExportTitle(queryModel);
+    const title = ExportUi.generateExportTitle(queryModel);
     document.title = 'Huey - ' + title;
 
     Routing.updateRouteFromQueryModel(queryModel);
   }, null, 50);
 
-  var tupleNumberFormatter = createNumberFormatter(0).format;
-  pivotTableUi.addEventListener('updated', async function(e){
-    var eventData = e.eventData;
-    var status = eventData.status;
+  const tupleNumberFormatter = createNumberFormatter(0).format;
+  pivotTableUi.addEventListener('updated', async event => {
+    const eventData = event.eventData;
+    const status = eventData.status;
     
-    var numRowsTuples = '';
-    var numColumnsTuples = '';
+    let numRowsTuples = '';
+    let numColumnsTuples = '';
     
     switch (status) {
       case 'error':
         showErrorDialog(eventData.error);
         break;
       case 'success':
-        var tupleCounts = eventData.tupleCounts;
+        const tupleCounts = eventData.tupleCounts;
+        const cellsInfo = tupleCounts[QueryModel.AXIS_CELLS];
 
-        var cellsInfo = tupleCounts[QueryModel.AXIS_CELLS];
-
-        var numRowsTuples = tupleCounts[QueryModel.AXIS_ROWS];
+        numRowsTuples = tupleCounts[QueryModel.AXIS_ROWS];
         numRowsTuples = typeof numRowsTuples === 'number' ? tupleNumberFormatter(numRowsTuples) : '';
         if (cellsInfo.count > 1 && cellsInfo.axis === QueryModel.AXIS_ROWS) {
           numRowsTuples += ` × ${cellsInfo.count}`;
         }
         
-        var numColumnsTuples = tupleCounts[QueryModel.AXIS_COLUMNS];
+        numColumnsTuples = tupleCounts[QueryModel.AXIS_COLUMNS];
         numColumnsTuples = typeof numColumnsTuples === 'number' ? tupleNumberFormatter(numColumnsTuples) : '';
         if (cellsInfo.count > 1 && cellsInfo.axis === QueryModel.AXIS_COLUMNS) {
           numColumnsTuples += ` × ${cellsInfo.count}`;
@@ -212,18 +218,13 @@ function initApplication(){
     byId('queryResultColumnsInfo').textContent = numColumnsTuples;
   });
 
-  bufferEvents(pivotTableUi, 'busy', function(event, count){
+  bufferEvents(pivotTableUi, 'busy', (event, count) => {
     if (count !== undefined) {
       return;
     }
-    var busy = event.eventData.busy;
-    var busyDialog = byId('visualizationProgressDialog');
-    if (busy) {
-      busyDialog.showModal();
-    }
-    else {
-      busyDialog.close();
-    }
+    const busy = event.eventData.busy;
+    const busyDialog = byId('visualizationProgressDialog');
+    busyDialog[busy ? 'showModal' : 'close']();
   });
 
   initPostMessageInterface();
